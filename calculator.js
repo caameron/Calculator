5c4ad6f312fd041d1ad85f21fbc7c82b4db34ac0
caameron <caamnaka@gmail.com>
2022-11-06 17:15:51 -0800

    First operand works. Need to work on chaining them together.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,51 @@
+let topScreenDisplayText = "";
+let topScreenDisplay = document.getElementById("top-screen");
+let mainScreenDisplayText = "";
+let mainScreenDisplay = document.getElementById("main-screen");
+let leftNumber = null;
+let rightNumber = null;
+let rightNumberDisplay = "";
 
+const numberButtons = Array.from(document.querySelectorAll('.number'));
+numberButtons.forEach(number => number.addEventListener('click', () => displayNumberOnScreen(number.innerHTML)));
+
+const operatorButtons = Array.from(document.querySelectorAll('.operator'));
+operatorButtons.forEach(operator => operator.addEventListener('click', () => operatorSelected(operator.innerHTML)));
+
+function displayNumberOnScreen(number) {
+    if(leftNumber !== null){
+        rightNumberDisplay += number;
+    }
+    topScreenDisplayText += number;
+    topScreenDisplay.innerHTML = topScreenDisplayText;
+}
+
+function operatorSelected(operator) {
+    //Save first number
+    if(leftNumber === null) {
+        leftNumber =  parseInt(topScreenDisplayText);
+        displayOperatorOnScreen(operator);
+    }
+    else {
+        rightNumber = parseInt(rightNumberDisplay);
+        leftNumber =  Calculate(operator, leftNumber, rightNumber);
+        mainScreenDisplayText = leftNumber;
+        mainScreenDisplay.innerHTML = mainScreenDisplayText;
+        topScreenDisplay.innerHTML = '';
+        topScreenDisplay.innerHTML = mainScreenDisplayText;
+        topScreenDisplayText = mainScreenDisplayText;
+        displayOperatorOnScreen(operator);
+    }
+
+    
+}
+
+function displayOperatorOnScreen(operator) {
+    topScreenDisplayText += ' ' + operator + ' ';
+    topScreenDisplay.innerHTML = topScreenDisplayText;
+}
+
+//Operator Functions
 function add(left, right) {
     return left + right;
 }
@@ -19,5 +66,18 @@ function operate(operator, left, right) {
     return operator(left, right);
 }
 
+function Calculate(operator, left, right) {
+    switch(operator) {
+        case '+':
+            return operate(add, left, right);
+        case '-':
+            return operate(subtract, left, right);
+        case 'x': 
+            return operate(multiply, left, right);
+        case '/':
+            return operate(divide, left, right);
+        default: 
+            return 0;
+    }
 
-// console.log(operate(multiply, 2, 3));
+}
